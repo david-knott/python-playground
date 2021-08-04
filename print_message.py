e6cef63b80a7c825c4619593e3e69fd244c7b9a5
Bonzo Dog <david-knott@users.noreply.github.com>
2021-08-04 09:20:43 +0100

    Update print_message.py

--- print_message.py
+++ print_message.py
@@ -1,7 +1,20 @@
+#!/usr/bin/env python
+
 # use slice and int.from_bytes to extract data from message
 # array slice https://stackoverflow.com/questions/509211/understanding-slice-notation
 # https://www.rapidtables.com/convert/number/hex-to-decimal.html
 # https://www.programiz.com/python-programming/online-compiler/
+
+# data = b'\xff\x00\xff\x00\xff\x00'
+# data = bytearray([0x7f,0x7f,0x7c,0xd7,0x13,0x0a,0x55,0xc3])
+# print_message(data)
+
+
+
+from __future__ import print_function
+
+import can
+# from can.bus import BusState
 
 def print_message(test):
     """prints out canbus message values"""
@@ -11,6 +24,21 @@ def print_message(test):
     sodec = int.from_bytes(test[6:7], byteorder='big')
     print('hv =', hcvdec * 0.0001, 'lv =', lcvdec * 0.0001, 'pv =', pvdec* 0.01, 'so =', sodec* 0.5)
 
-# data = b'\xff\x00\xff\x00\xff\x00'
-data = bytearray([0x7f,0x7f,0x7c,0xd7,0x13,0x0a,0x55,0xc3])
-print_message(data)
+    
+def receive_all():
+    bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
+    # bus.state = BusState.ACTIVE  # or BusState.PASSIVE
+    try:
+        while True:
+            msg = bus.recv(1)
+            if msg is not None:
+                print_message(msg.data)
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    receive_all()
+
+    
+
+
